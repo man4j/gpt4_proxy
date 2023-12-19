ca3c09f0946dedce7505ab7ed762725d8db64fc1
Vladimir Petrukhin <man4j@ya.ru>
2023-12-19 17:54:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,47 @@
-from typing import Optional
+import fastapi
+import uvicorn
+import openai
+import os
+from pydantic import BaseModel
 
-from fastapi import FastAPI
 
-app = FastAPI()
+client = openai.OpenAI()
+app = fastapi.FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class TextRequest(BaseModel):
+    text: str
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+
+@app.post("/summarize", name = "Проанализировать бирку", tags = ["GPT4"])
+def generate(request: TextRequest):
+    response = client.chat.completions.create(
+      model="gpt-4-1106-preview",
+      messages=[
+        {
+          "role": "system",
+          "content": "Опиши состав одежды построчно. Ответ напиши на русском языке."
+        },
+        {
+          "role": "user",
+          "content": request.text
+        }
+      ],
+      temperature=0,
+      max_tokens=1024,
+      top_p=0,
+      frequency_penalty=0,
+      presence_penalty=0
+    )
+    return {"result": response.choices[0].message.content}
+
+
+if __name__ == "__main__":
+    config = uvicorn.Config("main:app",
+                            port=8040,
+                            host="0.0.0.0",
+                            log_level="info",
+                            workers=os.cpu_count())
+
+    server = uvicorn.Server(config)
+    server.run()
